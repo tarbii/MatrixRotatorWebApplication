9742efe5c35120c58f8a0622fbd6adcd4b104105
Taras Babii <taras.babii@gmail.com>
2017-03-19 14:17:59 +0200

    Matrix model added and passed via controlled to view table

--- src/MatrixRotatorWebApplication/Controllers/MatrixController.cs
+++ src/MatrixRotatorWebApplication/Controllers/MatrixController.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MatrixRotatorWebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MatrixRotatorWebApplication.Controllers
 {
     public class MatrixController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var size = 7;
+            var elements = new[,]
+            {
+                {1, 2, 3, 4, 5, 6, 7},
+                {8, 9, 3, 2, 7, 2, 6},
+                {4, 6, 9, 8, 7, 2, 1},
+                {7, 1, 3, 5, 8, 9, 3},
+                {2, 3, 4, 6, 7, 8, 2},
+                {5, 1, 2, 3, 4, 7, 9},
+                {4, 1, 2, 8, 7, 6, 1}
+            };
+            var matrix = new Matrix<int>(size) {Elements = elements};
+            return View(matrix);
         }
     }
 }
---
+++ src/MatrixRotatorWebApplication/Models/Matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatrixRotatorWebApplication.Models
+{
+    public class Matrix<T>
+    {
+        public int Size { get; set; }
+
+        public T[,] Elements { get; set; }
+
+        public Matrix(int size)
+        {
+            Size = size;
+        }
+    }
+}
