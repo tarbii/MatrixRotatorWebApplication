c06663ca3678655bb75a6638f14a2347e7246dee
Taras Babii <taras.babii@gmail.com>
2017-03-19 12:54:17 +0200

    Startup changed to Matrix

--- src/MatrixRotatorWebApplication/Startup.cs
+++ src/MatrixRotatorWebApplication/Startup.cs
@@ -53,7 +53,7 @@ namespace MatrixRotatorWebApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Matrix}/{action=Index}/{id?}");
             });
         }
     }
