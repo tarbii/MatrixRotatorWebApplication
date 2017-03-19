f7d008b67b2a390de43af2555c3c09a004129984
Taras Babii <taras.babii@gmail.com>
2017-03-19 12:39:43 +0200

    MatrixController added

---
+++ src/MatrixRotatorWebApplication/Controllers/MatrixController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatrixRotatorWebApplication.Controllers
+{
+    public class MatrixController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
