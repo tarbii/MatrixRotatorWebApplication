0bed2d402a27f6dae00ed1941bb85b8eaff2f07b
Taras Babii <taras.babii@gmail.com>
2017-03-23 22:37:18 +0200

    MatrixBinder added (not working). View changed for custom names for cells (for binding)

---
+++ src/MatrixRotatorWebApplication/Models/MatrixBinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MatrixRotatorWebApplication.Models
+{
+    public class MatrixBinder<T> : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            bindingContext.Result = ModelBindingResult.Success(new int?[,] { { 45 } });
+            return Task.CompletedTask;
+        }
+    }
+}
