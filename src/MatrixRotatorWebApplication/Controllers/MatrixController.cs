39a216b8cb2bcd465d8b9b307438d643cf05ae2b
Taras Babii <taras.babii@gmail.com>
2017-03-24 10:36:43 +0200

    Changing matrix size implemented

--- src/MatrixRotatorWebApplication/Controllers/MatrixController.cs
+++ src/MatrixRotatorWebApplication/Controllers/MatrixController.cs
@@ -11,18 +11,8 @@ namespace MatrixRotatorWebApplication.Controllers
     {
         public IActionResult Index()
         {
-            var size = 7;
-            var elements = new int?[,]
-            {
-                {1, 2, 3, 4, 5, 6, 7},
-                {8, 9, 3, 2, 7, 2, 6},
-                {4, 6, 9, 8, 7, 2, 1},
-                {7, 1, 3, 5, 8, 9, 3},
-                {2, 3, 4, 6, 7, 8, 2},
-                {5, 1, 2, 3, 4, 7, 9},
-                {4, 1, 2, 8, 7, 6, 1}
-            };
-            var matrix = new Matrix<int?>(size) {Elements = elements};
+            const int size = 5;
+            var matrix = new Matrix<int?>(size) { Elements = new int?[size, size] };
             return View(matrix);
         }
 
