f368f3b3796f71b79f537b2833f3fbbe7efd73f8
Taras Babii <taras.babii@gmail.com>
2017-03-24 13:55:16 +0200

    MatrixRotator class library added

---
+++ MatrixRotator/MatrixRotator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatrixRotator
+{
+    public static class MatrixRotator
+    {
+        public static void RotateMatrix<T>(this T[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
