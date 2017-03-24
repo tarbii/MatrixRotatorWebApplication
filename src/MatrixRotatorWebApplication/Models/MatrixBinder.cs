44082f53e5b8c07dd41a80ae8117bb10f81f63e6
Taras Babii <taras.babii@gmail.com>
2017-03-24 11:11:50 +0200

    Hidden size added to submit for to make binding work

--- src/MatrixRotatorWebApplication/Models/MatrixBinder.cs
+++ src/MatrixRotatorWebApplication/Models/MatrixBinder.cs
@@ -14,7 +14,7 @@ namespace MatrixRotatorWebApplication.Models
             for (var i = 0; i < size; i++)
             for (var j = 0; j < size; j++)
                 elements[i, j] = BindingDeserializer.Deserialize<T>(
-                    bindingContext.ValueProvider.GetValue($"elements[{i}, {j}]").FirstValue);
+                    bindingContext.ValueProvider.GetValue($"Elements[{i}, {j}]").FirstValue);
             var matrix = new Matrix<T>(size) {Elements = elements};
             bindingContext.Result = ModelBindingResult.Success(matrix);
             return Task.CompletedTask;
