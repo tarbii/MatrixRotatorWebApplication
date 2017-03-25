93b9385e2a94745dcf91881ed9dd18b2f9290933
Taras Babii <taras.babii@gmail.com>
2017-03-25 12:17:41 +0200

    RotateMatrix<T> method implemented. Tests pass

--- MatrixRotator/MatrixRotator.cs
+++ MatrixRotator/MatrixRotator.cs
@@ -9,7 +9,20 @@ namespace MatrixRotator
     {
         public static void RotateMatrix<T>(this T[,] matrix)
         {
-            throw new NotImplementedException();
+            var n = matrix.GetLength(0);
+            for (var i = 0; i < n / 2; i++)
+            {
+                for (var j = i; j < n - i - 1; j++)
+                {
+                    var a = matrix[j, n - i - 1];
+                    matrix[j, n - i - 1] = matrix[i, j];
+                    var b = matrix[n - i - 1, n - j - 1];
+                    matrix[n - i - 1, n - j - 1] = a;
+                    a = matrix[n - j - 1, i];
+                    matrix[n - j - 1, i] = b;
+                    matrix[i, j] = a;
+                }
+            }
         }
     }
 }
